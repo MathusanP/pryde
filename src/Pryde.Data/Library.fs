15fd42b34bb98318717131d7f76b14286be6be67
mathusan paramanithy <mathuyt141@gmail.com>
2026-07-25 21:30:33 +0100

    feat(WindowsMetrics.fs): Added Processor Metrics
    Load, Processor Count, Name

--- src/Pryde.Data/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Pryde.Data
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
---
+++ src/Pryde.Data/WindowsMetrics.fs
@@ -0,0 +1,29 @@
+﻿namespace Pryde.Data
+
+open Pryde.Core
+open System.Management
+
+module WindowsMetrics =
+    let getCpuUsage () : CpuMetrics =
+        use searcher = new ManagementObjectSearcher("SELECT LoadPercentage, NumberOfLogicalProcessors, Name FROM Win32_Processor")
+        let results = searcher.Get()
+
+        let mutable totalLoad = 0.0
+        let mutable coreCount = 0
+        let mutable count = 0
+        let mutable name = ""
+
+        for item in results do
+            let load = item.["LoadPercentage"] :?> uint16
+            let cores = item.["NumberOfLogicalProcessors"] :?> uint32
+            let itemName = item.["Name"] :?> string
+            totalLoad <- totalLoad + float load
+            coreCount <- coreCount + int cores
+            count <- count + 1
+            name <- itemName
+
+        {
+            UsagePercent = if count > 0 then totalLoad / float count else 0.0
+            CoreCount = coreCount
+            Name = name
+        }
