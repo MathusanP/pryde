1998e0ab403b6cb8e3bf603f21765be3befa3656
mathusan paramanithy <mathuyt141@gmail.com>
2026-08-22 20:52:26 +0100

    feat: Made Http endpoints

---
+++ src/Pryde.Api/Handlers.fs
@@ -0,0 +1,30 @@
+﻿module Pryde.Api.Handlers
+
+open Giraffe
+open Pryde.Data
+
+let getCpuHandler : HttpHandler =
+    fun next ctx ->
+        json (DeviceMetrics.getCpuInfo ()) next ctx
+
+let getDiskHandler : HttpHandler =
+    fun next ctx ->
+        json (DeviceMetrics.getDiskInfo ()) next ctx
+
+let getGraphicsHandler : HttpHandler =
+    fun next ctx ->
+        json (DeviceMetrics.getGraphicsInfo ()) next ctx
+
+let getUptimeHandler : HttpHandler =
+    fun next ctx ->
+        json (DeviceMetrics.getUptime ()) next ctx
+
+let getNetworkThroughputHandler : HttpHandler =
+    fun next ctx ->
+        json (NetworkMetrics.getNetworkThroughput ()) next ctx
+
+let getPortHandler (port: int) : HttpHandler =
+    fun next ctx ->
+        match NetworkMetrics.getPortInfo port with
+        | Some info -> json info next ctx
+        | None -> RequestErrors.NOT_FOUND (text "Port not listening") next ctx
