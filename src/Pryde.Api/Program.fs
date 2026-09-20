46283dd002468214880bf09777d410c781d57118
mathusan paramanithy <mathuyt141@gmail.com>
2026-09-20 10:09:28 +0100

    feat(RouteTests.fs): Added routes testing

--- src/Pryde.Api/Program.fs
+++ src/Pryde.Api/Program.fs
@@ -1,15 +1,22 @@
+namespace Pryde.Api
+
 open Giraffe
 open Pryde.Api.Routes
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.DependencyInjection
 
-[<EntryPoint>]
-let main args =
-    let builder = WebApplication.CreateBuilder(args)
-    builder.Services.AddGiraffe() |> ignore
-    
-    let app = builder.Build()
-    app.UseGiraffe(webApp)
-    
-    app.Run()
-    0  // Exit code
+
+type Program() =
+    class end
+
+module Program =     
+    [<EntryPoint>]
+    let main args =
+        let builder = WebApplication.CreateBuilder(args)
+        builder.Services.AddGiraffe() |> ignore
+        
+        let app = builder.Build()
+        app.UseGiraffe(webApp)
+        
+        app.Run()
+        0  // Exit code
