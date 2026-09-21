3565ace8ef9496ab07f45da7b68e42148433fb1f
mathusan paramanithy <mathuyt141@gmail.com>
2026-09-21 01:05:27 +0100

    feat: Added windows service

--- src/Pryde.Api/Program.fs
+++ src/Pryde.Api/Program.fs
@@ -3,6 +3,9 @@ namespace Pryde.Api
 open Giraffe
 open Pryde.Api.Routes
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.DependencyInjection
 
 
@@ -13,6 +16,7 @@ module Program =
     [<EntryPoint>]
     let main args =
         let builder = WebApplication.CreateBuilder(args)
+        builder.Host.UseWindowsService() |> ignore
         builder.Services.AddGiraffe() |> ignore
         
         let app = builder.Build()
